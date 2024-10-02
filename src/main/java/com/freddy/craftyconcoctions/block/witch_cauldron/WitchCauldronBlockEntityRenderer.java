3dd747f926b65ca4899dfe5e5aa3fabb20f608a7
FreddyMSchubert <github@frederickschubert.de>
2024-10-02 18:52:28 +0200

    Ingredient rendering

--- src/main/java/com/freddy/craftyconcoctions/block/witch_cauldron/WitchCauldronBlockEntityRenderer.java
+++ src/main/java/com/freddy/craftyconcoctions/block/witch_cauldron/WitchCauldronBlockEntityRenderer.java
@@ -1,20 +1,38 @@
 package com.freddy.craftyconcoctions.block.witch_cauldron;
 
 import com.freddy.craftyconcoctions.CraftyConcoctions;
+import com.freddy.craftyconcoctions.util.ItemDisplayProperties;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationAxis;
 import org.joml.Matrix4f;
 
 public class WitchCauldronBlockEntityRenderer implements BlockEntityRenderer<WitchCauldronBlockEntity>
 {
+    private static final float pixel = 1f / 16f;
+
     private static final int idleFrames = 32;
+
+    private static final float itemScale = 0.25f;
+
+    private static final ItemDisplayProperties[] ITEM_DISPLAY_PROPERTIES = new ItemDisplayProperties[]
+    {
+            new ItemDisplayProperties(0.5f, 0f, 0.125f + pixel, -22.5f, 0f, 0f), // North side
+            new ItemDisplayProperties(0.875f - pixel, 0f, 0.5f, -22.5f, 90f, 0f), // East side
+            new ItemDisplayProperties(0.5f, 0f, 0.875f - pixel, -22.5f, 180f, 0f), // South side
+            new ItemDisplayProperties(0.125f + pixel, 0f, 0.5f, -22.5f, 270f, 0f), // West side
+    };
 
     public WitchCauldronBlockEntityRenderer(BlockEntityRendererFactory.Context context) {
     }
@@ -39,5 +57,58 @@ public class WitchCauldronBlockEntityRenderer implements BlockEntityRenderer<Wit
         vertexConsumer.vertex(matrixPos, -0.5f, -0.5f, 0.0f).color(WitchCauldronSettings.WATER_COLOR.RED, WitchCauldronSettings.WATER_COLOR.GREEN, WitchCauldronSettings.WATER_COLOR.BLUE, WitchCauldronSettings.WATER_COLOR.ALPHA).texture(0.0f, 1.0f).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(entry, 0.0f, 1.0f, 0.0f);
 
         matrices.pop();
+
+
+        // 2. Render ingredients
+        ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
+        Direction facing = entity.getCachedState().get(WitchCauldronBlock.FACING);
+
+        for (int i = 0; i < entity.ingredients.size(); i++)
+        {
+            ItemDisplayProperties props = ITEM_DISPLAY_PROPERTIES[i];
+            ItemStack itemStack = entity.ingredients.get(i).getDefaultStack();
+            if (itemStack.isEmpty()) continue;
+
+            matrices.push();
+
+            float adjustedPosX = props.posX;
+            float adjustedPosY = props.posY;
+            float adjustedPosZ = props.posZ;
+            float adjustedRotY = props.rotY;
+
+            adjustedPosY = adjustedPosY + 0.2f + entity.waterAmount * 0.2f;
+
+            switch(facing)
+            {
+                case EAST:
+                    adjustedPosX = props.posZ;
+                    adjustedPosZ = 1.0f - props.posX;
+                    adjustedRotY = props.rotY - 90f;
+                    break;
+                case SOUTH:
+                    adjustedPosX = 1.0f - props.posX;
+                    adjustedPosZ = 1.0f - props.posZ;
+                    adjustedRotY = props.rotY - 180f;
+                    break;
+                case WEST:
+                    adjustedPosX = 1.0f - props.posZ;
+                    adjustedPosZ = props.posX;
+                    adjustedRotY = props.rotY + 90f;
+                    break;
+                case NORTH:
+                default:
+                    break;
+            }
+
+            matrices.translate(adjustedPosX, adjustedPosY, adjustedPosZ);
+            matrices.multiply(RotationAxis.NEGATIVE_Y.rotationDegrees(adjustedRotY));
+            matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(props.rotX));
+            matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(props.rotZ));
+            matrices.scale(itemScale, itemScale, itemScale);
+
+            itemRenderer.renderItem(itemStack, ModelTransformationMode.GUI, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, entity.getWorld(), 1);
+
+            matrices.pop();
+        }
     }
 }
---
+++ src/main/java/com/freddy/craftyconcoctions/util/ItemDisplayProperties.java
@@ -0,0 +1,17 @@
+package com.freddy.craftyconcoctions.util;
+
+public class ItemDisplayProperties
+{
+    public final float posX, posY, posZ;
+    public final float rotX, rotY, rotZ;
+
+    public ItemDisplayProperties(float posX, float posY, float posZ, float rotX, float rotY, float rotZ)
+    {
+        this.posX = posX;
+        this.posY = posY;
+        this.posZ = posZ;
+        this.rotX = rotX;
+        this.rotY = rotY;
+        this.rotZ = rotZ;
+    }
+}
